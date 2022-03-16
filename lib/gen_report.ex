11c142d8e255e4629f7d56a802ceb98dbcfb3a01
saviobarbosa <saviiobarbosa@gmail.com>
2022-03-16 19:30:45 -0300

    Finalizando o desafio

--- lib/gen_report.ex
+++ lib/gen_report.ex
@@ -1,6 +1,44 @@
 defmodule GenReport do
+  alias GenReport.{Merger, Parser}
 
-  def build do
-    # TO DO
+  def build(filename) do
+    {:ok, result} =
+      filename
+      |> Parser.parse_file()
+
+    Enum.reduce(result, initial_acc(), fn line, report ->
+      organize_data(line, report)
+    end)
+  end
+
+  defp organize_data(
+         [name, hour, _day, month, year],
+         %{
+           :all_hours => all_hours,
+           :hours_per_month => hours_per_month,
+           :hours_per_year => hours_per_year
+         }
+       ) do
+    all_hours = Merger.merge(all_hours, %{name => hour})
+
+    hours_per_month = Merger.merge_two_levels(hours_per_month, %{name => %{month => hour}})
+
+    hours_per_year = Merger.merge_two_levels(hours_per_year, %{name => %{year => hour}})
+
+    %{"all_hours" => all_hours, "hours_per_month" => %{}, "hours_per_year" => %{}}
+
+    build_report(all_hours, hours_per_month, hours_per_year)
+  end
+
+  defp initial_acc() do
+    build_report(%{}, %{}, %{})
+  end
+
+  defp build_report(all_hours, hours_per_month, hours_per_year) do
+    %{
+      :all_hours => all_hours,
+      :hours_per_month => hours_per_month,
+      :hours_per_year => hours_per_year
+    }
   end
 end
